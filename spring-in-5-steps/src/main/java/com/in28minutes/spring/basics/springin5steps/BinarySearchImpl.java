9693ba8f66fc584298cbacdb0ef6540e84567725
amandhakal79 <38598487+amandhakal79@users.noreply.github.com>
2019-08-23 20:48:52 +0545

    Bubble sort and Quick sort selected via main class

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,13 +1,43 @@
 package com.in28minutes.spring.basics.springin5steps;
 
 public class BinarySearchImpl {
+	private SortAlgorithm sortAlgorithm;
 	
 	public int binarySearch(int [] numbers, int numberToSearchFor) {
 		//sorting an array
 		//search the array
-		//return the result
+		//return the result 
+		
+		//lets assume 
+		            // this search is using "Bubble sort algorithm"
+		
+		//it is tightly coupled with bubble sort algorithm
+		//because its using bubble sort algorithm
+		//this is not good
+		  
+		//if I want to use "Quick Sort Algorithm", 
+		     //i need to change entire logic of this program
+		
+		
+		//so i created BubbleSortAlgorithm separately
+	
+
+		
+		
+		
+	//	BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+		
+		
+		
+		int [ ] sortedNumbers =sortAlgorithm.sort(numbers);
+		System.out.println(sortAlgorithm);
 		return 3; //to make compiler happy	
 	}
+
+	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+		super();
+		this.sortAlgorithm = sortAlgorithm;
+	} 
 	
 	
 }
---
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -0,0 +1,17 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+public class BubbleSortAlgorithm  implements SortAlgorithm{
+
+	//Instead of replacing code every time in
+	      //"BINARYSEARCHIMPL"
+	//I have separated this class 
+	   //to implement BubbleSortAlgorithm
+	
+	public int [] sort (int [ ] numbers ) {
+		//logic for bubble sort 
+		return numbers;
+	}
+	
+	
+	
+}
